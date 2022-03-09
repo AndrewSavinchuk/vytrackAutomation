2856088a96489e2ec7860f1b000c8531d0fb2c3a
AndrewSavinchuk <savawise@gmail.com>
2022-03-08 23:05:50 -0600

    Vytrack US69 automation complete

--- src/test/java/com/vytrack/utilities/VytrackUtils.java
+++ src/test/java/com/vytrack/utilities/VytrackUtils.java
@@ -1,10 +1,17 @@
 package com.vytrack.utilities;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
 
 public class VytrackUtils {
 
-    public static void login(String username,String password){
+
+    //private static String username = ConfigurationReader.getProperty("username1");
+    private static String password = ConfigurationReader.getProperty("password");
+
+
+    public static void login(String username, String password) {
         //go to website
         Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
         //pass username
@@ -13,39 +20,53 @@ public class VytrackUtils {
         Driver.getDriver().findElement(By.id("prependedInput2")).sendKeys(password);
         // click login button
         Driver.getDriver().findElement(By.xpath("//button[@id='_submit']")).click();
+
     }
 
-    public static void loginAsDriver(){
+    public static void loginAsDriver(String username) {
         //go to website
         Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
         //pass username
-        Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(ConfigurationReader.getProperty("driver_username"));
+        Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(username);
         //pass password
-        Driver.getDriver().findElement(By.id("prependedInput2")).sendKeys(ConfigurationReader.getProperty("driver_password"));
+        Driver.getDriver().findElement(By.id("prependedInput2")).sendKeys(password);
         // click login button
         Driver.getDriver().findElement(By.xpath("//button[@id='_submit']")).click();
+
+//        Select logOutDropDown = new Select(Driver.getDriver().findElement(By.xpath("//li[@id=\"user-menu\"]/a")));
+//        logOutDropDown.selectByVisibleText("Logout");
+
+
     }
 
-    public static void loginAsStoreManager(){
+    public static void loginAsStoreManager(String username) {
         //go to website
         Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
         //pass username
-        Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(ConfigurationReader.getProperty("store_manager_username"));
+        Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(username);
         //pass password
-        Driver.getDriver().findElement(By.id("prependedInput2")).sendKeys(ConfigurationReader.getProperty("store_manager_password"));
+        Driver.getDriver().findElement(By.id("prependedInput2")).sendKeys(password);
         // click login button
         Driver.getDriver().findElement(By.xpath("//button[@id='_submit']")).click();
+
     }
 
-    public static void loginAsSalesManager(){
+    public static void loginAsSalesManager(String username) {
         //go to website
         Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
         //pass username
-        Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(ConfigurationReader.getProperty("sales_manager_username"));
+        Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(username);
         //pass password
-        Driver.getDriver().findElement(By.id("prependedInput2")).sendKeys(ConfigurationReader.getProperty("sales_manager_password"));
+        Driver.getDriver().findElement(By.id("prependedInput2")).sendKeys(password);
         // click login button
         Driver.getDriver().findElement(By.xpath("//button[@id='_submit']")).click();
+
+    }
+
+
+    public static void logOut(){
+        Driver.getDriver().findElement(By.xpath("//li[@id=\"user-menu\"]/a")).click();
+        Driver.getDriver().findElement(By.xpath("//a[@class='no-hash']")).click();
     }
 
 
