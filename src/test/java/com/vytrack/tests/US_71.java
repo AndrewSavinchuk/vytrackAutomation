1f50af01460821d5cfdca4f8cbf2bae181eb47c4
Aziz <azizozen1992@yahoo.com>
2022-03-10 23:35:34 -0600

    My Initial Commit

---
+++ src/test/java/com/vytrack/tests/US_71.java
@@ -0,0 +1,12 @@
+package com.vytrack.tests;
+
+import com.vytrack.tests.base.TestBaseNew;
+import org.testng.annotations.DataProvider;
+
+public class US_71 extends TestBaseNew {
+
+    public void loginUsingDriver(){
+
+    }
+
+}
